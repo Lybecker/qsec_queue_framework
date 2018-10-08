cf02b8fd698b4dbb81f2fe3489788a902324b735
Chris Deaker <>
2018-10-08 15:03:32 +1300

    Initial implementation of basic priority queue functions.

--- src/QueueFramework/PriorityQueue.cs
+++ src/QueueFramework/PriorityQueue.cs
@@ -1,8 +1,41 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace QueueFramework
 {
-    public class PriorityQueue
+    public class PriorityQueue<T>
     {
+        private readonly List<KeyValuePair<int, T>> items = new List<KeyValuePair<int, T>>();        
+
+        public int Count => items.Count;
+
+        public void Enqueue(T item, int priority)
+        {
+            var newItemPair = new KeyValuePair<int, T>(priority, item);
+
+            var index = items.FindIndex(x => x.Key <= priority);
+            if (index >= 0)
+            {
+                items.Insert(index, newItemPair);
+            }
+            else items.Add(newItemPair);
+        }
+
+        public T Dequeue()
+        {
+            var result = Peek();
+            items.RemoveAt(Count - 1);
+            return result;
+        }
+
+        public T Peek()
+        {
+            if (items.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            return items[Count - 1].Value;
+        }
     }
 }
--- test/QueueFramework.Test/PriorityQueueTests.cs
+++ test/QueueFramework.Test/PriorityQueueTests.cs
@@ -5,10 +5,36 @@ namespace QueueFramework.Test
 {
     public class PriorityQueueTests
     {
+
         [Fact]
-        public void Test1()
+        public void NewCollection_IsEmpty()
         {
-            Assert.True(true);
+            var queue = new PriorityQueue<string>();
+            Assert.Equal(0, queue.Count);
+        }
+
+        [Fact]
+        public void Enqueue_IncrementsCount()
+        {
+            var queue = new PriorityQueue<string>();
+            for (int i = 1; i < 10; i++) {
+                queue.Enqueue("test" + i, i);
+                Assert.Equal(i, queue.Count);
+            }
+        }
+
+        [Fact]
+        public void Dequeue_ThrowsExceptionForEmptyQueue()
+        {
+            var queue = new PriorityQueue<string>();
+            Assert.Throws<InvalidOperationException>(() => queue.Dequeue());
+        }
+
+        [Fact]
+        public void Peek_ThrowsExceptionForEmptyQueue()
+        {
+            var queue = new PriorityQueue<string>();
+            Assert.Throws<InvalidOperationException>(() => queue.Peek());
         }
     }
 }
